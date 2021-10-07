8d5202cb81977277974ea349af123a4efc4d5743
“maria-luisa-gomez” <mluisa.gomezdeaguero@gmail.com>
2021-10-07 22:35:51 +0200

    0710201_2

--- streamlit/risk_prediction/main.py
+++ streamlit/risk_prediction/main.py
@@ -30,7 +30,10 @@ imagen = Image.open("images/severity_risk.png")
 st.image(imagen, use_column_width=True)
 
 
-uploaded_file = st.file_uploader("Insert pacients profile", type = ['csv'])
+st.write("""
+## Insertar datos de los pacientes
+""")
+uploaded_file = st.file_uploader("", type = ['csv'])
 if uploaded_file is not None:
     df1 = pd.read_csv(uploaded_file)
     df1.to_csv('probando.csv', index = False)
@@ -39,7 +42,6 @@ if uploaded_file is not None:
  
     st.subheader('Prediction Probability')
     st.table(prob_pred)
-
 
 file_ = open("images/virus3.gif", "rb")
 contents = file_.read()
@@ -50,5 +52,6 @@ st.markdown(
     f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
     unsafe_allow_html=True,
 )
-data = "images/vaccine_animation.mp4"
-st.video(data, format="video/mp4")
+
+#data = "images/vaccine_animation.mp4"
+#st.video(data, format="video/mp4")
